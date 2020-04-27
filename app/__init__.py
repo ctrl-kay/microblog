a046d01af0a2ecf92e316a0890efa9ad282eb971
ctrl-kay <kaylan.d.horne@gmail.com>
2020-04-27 14:49:39 -0700

    Created database branch. Started DB work, added imports to init file and variables to config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask  # import flask class
 from config import Config # from config module import Config class
+from flask_sqlalchemy import SQAlchemy # from sqlalchemy module import SQAlchemy object
+from flask_migrate import Migrate #import Migrate class from migrate package
 
 app = Flask(__name__) # instantiate flask object
 app.config.from_object(Config) # read and apply config file
 
-from app import routes # import routes file, always have this below
+from app import routes # import routes, models file, always have this below
 
--- config.py
+++ config.py
@@ -1,7 +1,13 @@
-import os
-
 # Config class to set and store variables #
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__)) #base directory variable
 
 # Look for value of SECRET_KEY enviroment variable,  or use hard coded string #
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # secret key variable
+
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'app.db') #if variable not defined,use app.db in main directory
+		# which is stored in the basedir
+	SQLALCHEMY_TRACK_MODIFICATIONS = False # stop signaling app when db is updated
