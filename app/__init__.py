35d71d1ef20e69b0d485ba2313514b76f88e4a84
ctrl-kay <59586282+ctrl-kay@users.noreply.github.com>
2020-05-07 14:57:11 -0700

    Added register form, Added Database functionality, updated files to accomodate additions

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask  # import flask class
 from config import Config # from config module import Config class
 from flask_sqlalchemy import SQLAlchemy # from sqlalchemy module import SQAlchemy object
 from flask_migrate import Migrate #import Migrate class from migrate package
+from flask_login import LoginManager, login_required 
 
 app = Flask(__name__) # instantiate flask object
 app.config.from_object(Config) # read and apply config file
 db = SQLAlchemy(app) # db object to represent the database
 migrate = Migrate(app, db) # migrate object to represent Migrate engine
+login = LoginManager(app) #Initialize flask login module 
+login.login_view = 'login' # url_for endpoint for login view 
 
 from app import routes, models # import routes, models file, always have this below
 
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 # Web Form Classes files
 from flask_wtf import FlaskForm # import form class from flask_wtf 
 from wtforms import StringField, PasswordField, BooleanField, SubmitField # import field types
-from wtforms.validators import DataRequired # import validation behaviors
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo # import validation behaviors
+from app.models import User
 
 # Define the Login form
 class LoginForm(FlaskForm):
@@ -9,3 +10,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+#  Registration form
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField(
+	'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+		    raise ValidationError('Please use a different username.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+		    raise ValidationError('Please use a different email address.')
